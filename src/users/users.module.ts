3b10a0b2f451445eff1ad05c87cef16d5e77473e
Ade Suluh N <ade.suluh@gmail.com>
2023-01-04 13:05:39 +0700

    auth service done

---
+++ src/users/users.module.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService, 
+              AuthService, 
+              {
+                provide: APP_INTERCEPTOR,
+                useClass: CurrentUserInterceptor
+              }
+            ]
+})
+export class UsersModule {}
