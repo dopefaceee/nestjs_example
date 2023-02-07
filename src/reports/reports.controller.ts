3c9bc7d9e7b1f98ab82ac725cd0e5ef515a92b54
Ade Suluh N <ade.suluh@gmail.com>
2023-02-07 15:11:48 +0700

    report done

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,8 @@
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { AdminGuard } from './../guards/admin.guard';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CurrentUser } from 'src/users/decorators/current-user.decorator';
@@ -12,6 +13,11 @@ import { ApproveReportDto } from './dtos/approve-report.dto';
 @Controller('reports')
 export class ReportsController {
     constructor(private reportService: ReportsService) {}
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto) {
+       return this.reportService.createEstimate(query)
+    }
     
     @Post()
     @UseGuards(AuthGuard)
